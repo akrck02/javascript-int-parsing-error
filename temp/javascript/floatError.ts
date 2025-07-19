0d438ca2020706e3acda1780b92bfe41fed6856f
akrck02 <43274508+akrck02@users.noreply.github.com>
2025-07-19 15:51:20 +0000

    Deploying to gh-pages from @ akrck02/javascript-int-parsing-error@7d4ef6f5572b083880c5ca65c4ec19e998451442 🚀

--- temp/javascript/floatError.ts
+++
@@ -1,5 +0,0 @@
-
-// Typescript throws an error when trying to parse a float with a decimal point.
-// Is just so much better than javascript !!!!
-const error : number = 0.0000005; 
-const parsed = parseFloat(error);
